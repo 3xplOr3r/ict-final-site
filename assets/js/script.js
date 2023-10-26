401f3cbe9502386f42f5f0351a2124befac3a7ae
3xplOr3r <abiplob23@gmail.com>
2023-10-26 14:08:33 +0600

    done

--- assets/js/script.js
+++ assets/js/script.js
@@ -55,3 +55,69 @@ var swiper = new Swiper(".mySwiper", {
     loop: true,
     speed: 1200,
   });
+
+
+  // for hero section 
+
+  var slides = document.querySelectorAll(".slide");
+  var btns = document.querySelectorAll(".sl-btn");
+
+  let currentSlide = 1;
+// manual nav
+
+  var manualNav = function(manual) {
+    slides.forEach((slide) => {
+        slide.classList.remove('active');
+
+        btns.forEach((btn) => {
+            btn.classList.remove('active');
+        })
+
+    });
+
+
+    slides[manual].classList.add('active');
+    btns[manual].classList.add('active');
+
+  }
+
+  btns.forEach((btn, i) => {
+
+    btn.addEventListener("click" , () => {
+        manualNav(i);
+        currentSlide = i;
+    })
+
+  });
+
+
+
+
+  var Repeat = function(activePlay) {
+
+    let active = document.getElementsByClassName("active");
+    let i = 0;
+
+    var repeater = () => {
+        setTimeout(function() {
+
+            [...active].forEach((activeSlide) => {
+                activeSlide.classList.remove('active');
+            });
+
+            slides[i].classList.add('active');
+            btns[i].classList.add('active');
+            i++;
+
+            if(slides.length == i) {
+                i = 0;
+            }
+            if(i>=slides.length) {
+                return;
+            }
+            repeater();
+        }, 5000);
+    }
+    repeater();
+  }
+  Repeat();
